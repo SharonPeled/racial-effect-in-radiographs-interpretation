9cd6da832bc2285017b3c8eac07a024684630dc1
SharonPeled <ssharon95@gmail.com>
2022-07-15 12:06:24 +0300

    updating create_checkpoint fixing fast fowarding

--- CheXpert/shared_utils.py
+++ CheXpert/shared_utils.py
@@ -37,14 +37,22 @@ def vprint(s:str, TrainingConfigs, **print_kargs):
             file.flush()
 
 
+def start_training_msg(TrainingConfigs):
+    vprint('', TrainingConfigs) # newline
+    vprint("-" * 100, TrainingConfigs)
+    vprint("-" * 100, TrainingConfigs)
+    vprint('', TrainingConfigs) # newline
+    vprint("Start Training", TrainingConfigs)
+
+
 def get_time_str():
     time_str = str(datetime.datetime.now())[:-10]
     trans = str.maketrans("-: ","__-")
     return time_str.translate(trans)
 
 
-def create_checkpoint(model, epoch, i, valid_dataloader, criterion, results, TrainingConfigs, score_dict, by_study=None,
-                      challenge_ann_only=None):
+def create_checkpoint(model, optimizer, scheduler, criterion, epoch, i, valid_dataloader, results, TrainingConfigs,
+                                                             score_dict, by_study=None, challenge_ann_only=None):
     try:
         score_vals_dict = calc_scores(score_dict.keys(), model, valid_dataloader, TrainingConfigs, criterion,
                                             by_study, challenge_ann_only)
@@ -57,14 +65,19 @@ def create_checkpoint(model, epoch, i, valid_dataloader, criterion, results, Tra
         "epoch": epoch,
         "iter": i,
         "batch_size": TrainingConfigs.BATCH_SIZE,
-        "trainLastLoss": np.mean(results["train_loss"][-100:]),
+        "trainLastLoss": np.mean(results["train_loss"][-1]),
         "validAUC": results["valid_auc"][-1]
+    }
+    training_objects = {
+        "optimizer": optimizer,
+        "scheduler": scheduler,
+        "criterion": criterion
     }
     time_str = get_time_str()
     metadata_suffix = '__'.join([f"{k}-{round(v,4)}" for k, v in metadata.items()])
     filename = f"{time_str}__{TrainingConfigs.MODEL_VERSION}__{metadata_suffix}.dict"
     filepath = os.path.join(TrainingConfigs.CHECKPOINT_DIR, filename)
-    statedata = {**metadata, **{"model": model.state_dict(), "results": results}}
+    statedata = {**training_objects, **metadata, **{"model": model.state_dict(), "results": results}}
     torch.save(statedata, filepath)
     vprint(f"{time_str}: Checkpoint Created For {TrainingConfigs.MODEL_VERSION}.", TrainingConfigs)
     vprint('Epoch [%d/%d],   Iter [%d/%d],   Train Loss: %.4f,   Valid Loss: %.4f,   Valid AUC: %.4f'
@@ -135,7 +148,7 @@ def auc_score(labels, outputs, **kargs):
     return np.mean(AUROCs)
 
 
-def get_previous_training_place(model, TrainingConfigs):
+def get_previous_training_place(model, optimizer, scheduler, criterion, TrainingConfigs):
     if not os.path.isdir(TrainingConfigs.CHECKPOINT_DIR):
         os.mkdir(TrainingConfigs.CHECKPOINT_DIR)
     if TrainingConfigs.TRAINED_MODEL_PATH:
@@ -148,7 +161,7 @@ def get_previous_training_place(model, TrainingConfigs):
             "valid_loss": [-1],
             "valid_auc": [-1]
         }
-        return model, results, 0, -1
+        return model, optimizer, scheduler, criterion, results, 0, -1
     model_filename = files[-1]
     return load_statedict(model, os.path.join(TrainingConfigs.CHECKPOINT_DIR, model_filename), TrainingConfigs)
 
@@ -161,7 +174,8 @@ def load_statedict(model, path, TrainingConfigs):
         statedata = torch.load(path, map_location=torch.device('cuda'))
     model.load_state_dict(statedata['model'])
     statedata['model'] = model
-    return [statedata[k] for k in ['model', 'results', 'epoch', 'iter']]
+    return [statedata[k] for k in ['model', 'optimizer', 'scheduler', 'criterion', 'results', 'epoch', 'iter']]
+
 
 
 
