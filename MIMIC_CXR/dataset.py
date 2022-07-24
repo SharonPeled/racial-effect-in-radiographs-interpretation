f2949b7bf3ac8e370915dfc0723e215738101924
SharonPeled <ssharon95@gmail.com>
2022-07-24 18:16:31 +0300

    grad-cam

--- MIMIC_CXR/dataset.py
+++ MIMIC_CXR/dataset.py
@@ -6,6 +6,7 @@ from MIMIC_CXR.utils import Configs
 from MIMIC_CXR import utils
 from shared_utils import vprint, Mode
 import datetime
+from tqdm.notebook import tqdm
 from CheXpert.race_prediction.dataset import CheXpertRaceDataset
 from CheXpert.disease_prediction.dataset import CheXpertDiseaseDataset
 import shared_utils
@@ -30,6 +31,11 @@ class CXRDataset(GenericDataset):
     @classmethod
     def download_dataset(cls, group_sample_size, mode, data_dir, labels_filename, cxr_chexpert_labels_filename, admissions_filename,
                          split_filename, patients_filename, transform=None, target_transform=None):
+        """
+        :param group_sample_size: if int then sample from each group group_sample_size samples. If dict, then
+        should be full_group_str -> int, sample from each group different number of samples. The full_group_str should
+        be of form race_age_gender.
+        """
         df_cxp = pd.read_csv(os.path.join(data_dir, cxr_chexpert_labels_filename))
         df_adm = pd.read_csv(os.path.join(data_dir, admissions_filename))
         df_split = pd.read_csv(os.path.join(data_dir, split_filename))
@@ -43,8 +49,7 @@ class CXRDataset(GenericDataset):
         df_temp = (df_cxr_demo.groupby('subject_id')[['race', 'age', 'gender']].nunique() == 1).all(axis=1)
         valid_subject_ids = df_temp[df_temp.values].index
         df_cxr_demo = df_cxr_demo[df_cxr_demo.subject_id.isin(valid_subject_ids)].drop_duplicates()
-        df_cxr_demo = df_cxr_demo.groupby(['race', 'age', 'gender']).sample(n=group_sample_size,
-                                                                            replace=False, random_state=Configs.SEED)
+        df_cxr_demo = CXRDataset.sample(df_cxr_demo, group_sample_size)
         successes, failures = CXRDataset.download(data_dir, df_cxr_demo)
         df_cxr_demo = df_cxr_demo.merge(successes, on=['subject_id', 'study_id', 'dicom_id'])
         df_cxr_demo['img_path'] = df_cxr_demo.apply(lambda row: os.path.join(data_dir,
@@ -63,7 +68,7 @@ class CXRDataset(GenericDataset):
         folder_numbers = list(range(10,20))
         successes = []
         failures = []
-        for patient_id, study_id, dicom_id in df_cxr[['subject_id', 'study_id', 'dicom_id']].values:
+        for patient_id, study_id, dicom_id in tqdm(df_cxr[['subject_id', 'study_id', 'dicom_id']].values):
             done = False
             for folder_number in folder_numbers:
                 cmd = f"""wget -r -N -c -np --user {username} --password {password} -P {directory} https://physionet.org/files/mimic-cxr-jpg/2.0.0/files/p{folder_number}/p{patient_id}/s{study_id}/{dicom_id}.jpg"""
@@ -83,7 +88,19 @@ class CXRDataset(GenericDataset):
         vprint(f"MIMIC-CXR failed downloads: {len(failures)}", Configs)
         return successes, failures
 
-
-
-
+    @staticmethod
+    def sample(df_cxr_demo, group_sample_size):
+        if isinstance(group_sample_size, int):
+            return df_cxr_demo.groupby(['race', 'age', 'gender']).sample(n=group_sample_size,
+                                                                                replace=False,
+                                                                                random_state=Configs.SEED)
+        if not isinstance(group_sample_size, dict):
+            raise NotImplementedError
+        groups = [tuple(group.split('_')) for group in group_sample_size.keys()]
+        df_list = []
+        for attrs, df_group in df_cxr_demo.groupby(['race', 'age', 'gender']):
+            if attrs in groups:
+                key = '_'.join(attrs)
+                df_list.append(df_group.sample(group_sample_size[key], random_state=Configs.SEED))
+        return pd.concat(df_list, ignore_index=True)
 
